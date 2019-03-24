f3fa3e9b97b070d10205b88d9f09e3b2efcc26b2
Rob <rob.a.landry@gmail.com>
2019-03-23 20:47:08 -0400

    Pypi 0.1.1

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup
 
-setup(name='pynetgear_enhanced',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+      name='pynetgear_enhanced',
       version='0.1.1',
       description='Access Netgear routers using their SOAP API',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='http://github.com/roblandry/pynetgear_enhanced',
       download_url='http://github.com/roblandry/pynetgear_enhanced/archive/v0.1.1.tar.gz',
       author='Rob Landry',
@@ -10,4 +16,5 @@ setup(name='pynetgear_enhanced',
       license='MIT',
       install_requires=['requests>=2.0'],
       packages=['pynetgear_enhanced'],
-      zip_safe=True)
+      zip_safe=True
+)
